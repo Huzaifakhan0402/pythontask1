df938a5413732b2a02eef95ed6617718f5c72203
huzaifa khan <145002182+Huzaifakhan0402@users.noreply.github.com>
2023-10-30 17:39:18 +0530

    Add files via upload

---
+++ python 1/task1.py
@@ -0,0 +1,26 @@
+print("hello World 1")
+#it is single comment
+""" 
+this is multi line comment
+hccfubi
+hhiudkka
+"""
+#variable
+number = 25
+print(number)
+
+myNameIs = "huzaifa"
+word = 'T'
+
+print(myNameIs)
+print(word)
+
+#datatype
+
+num = 99 #integer
+alphabet = "khan" #string
+num1 = 99.909 #float
+bool = True #boolean
+type(bool)
+print(type(bool))
+
